caf50369903db86433842f7410f6c023e0056dff
ntwcklng <ntwcklng@gmail.com>
2017-10-15 14:19:35 +0200

    urls

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-var express = require('express');
-var app = express();
-
-const urls = {
-  "2jfGmmC": "http://amzn.to/2jfGmmC", //bsd
-  "2jfOC5V": "http://amzn.to/2jfOC5V", //duetto
-  "2wThyUz": "http://amzn.to/2wThyUz", //ibrid
-  "2eOBwaW": "http://amzn.to/2eOBwaW", //Q&S
-  "2xUgzTw": "http://amzn.to/2xUgzTw", //UC
-  "2xXxGo0": "http://amzn.to/2xXxGo0", //a5100
-  "8jfGmmC": "http://quickjack.de", //quickjack
-  "4xgrzTw": "https://hqs-shop.de/Meguiars-Ultimate-Compound-Lackreiniger-450ml?jsta=gb", //UC HQS
-  "7yTxFo0": "https://hqs-shop.de/ScanGrip-Colour-Match-SunMatch-2?jsta=gb", //SUNMATCH2
-}
+const express = require('express')
+const app = express()
+const Gists = require('gists')
+const gists = new Gists()
+let urls
 
 app.get('/:id', (req, res) => {
-  if (urls[req.params.id]) {
-    return res.redirect(urls[req.params.id])
-  }
-  res.send("Was machst du hier?")
-});
+  return gists.download({id: '5a12f330edd410e0c0e40af05e03803d'}, (err, result) => {
+    if (err) return err
+    const gistsUrls = result.files['urls'].content
+    .split('\n')
+    .map(line => line.split(',')
+    .filter(url => url.charAt(0) !== '#'))
+    urls = new Map(gistsUrls)
+    if (urls.has(req.params.id)) {
+      return res.redirect(urls.get(req.params.id))
+    }
+    res.send("Was machst du hier?")
+  })
+})
 
 app.get('/', (req, res) => {
   res.redirect('https://glossboss.de/')
@@ -26,4 +25,4 @@ app.get('/', (req, res) => {
 
 var server = app.listen(3000, function(){
   console.log('Server listening on port 3000');
-});
+})

3a40faaa91c10e45757b7b74626f305ed1543a82
ntwcklng <ntwcklng@gmail.com>
2018-11-17 09:23:51 +0100

    ?

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ const gists = new Gists()
 let urls
 
 app.get('/:id', (req, res) => {
-  return gists.download({id: '5a12f330edd410e0c0e40af05e03803d'}, (err, result) => {
-    if (err) return err
-    const gistsUrls = result.files['urls'].content
-    .split('\n')
-    .map(line => line.split(',')
-    .filter(url => url.charAt(0) !== '#'))
-    urls = new Map(gistsUrls)
-    if (urls.has(req.params.id)) {
-      return res.redirect(urls.get(req.params.id))
+  return gists.download(
+    { id: '5a12f330edd410e0c0e40af05e03803d' },
+    (err, result) => {
+      if (err) return err
+      const gistsUrls = result.files['urls'].content
+        .split('\n')
+        .map(line => line.split(',').filter(url => url.charAt(0) !== '#'))
+      urls = new Map(gistsUrls)
+      if (urls.has(req.params.id)) {
+        return res.redirect(urls.get(req.params.id))
+      }
+      res.send('Was machst du hier?')
     }
-    res.send("Was machst du hier?")
-  })
+  )
 })
 
 app.get('/', (req, res) => {
   res.redirect('https://glossboss.de/')
 })
 
-var server = app.listen(3000, function(){
-  console.log('Server listening on port 3000');
+var server = app.listen(3000, function() {
+  console.log('Server listening on port 3000')
 })
